b6f5c348c0d167cfb8f280f22fe7cae982519ea8
Miraj Maharjan <miraj.mhrjn@gmail.com>
2023-02-19 15:09:03 +0545

    tried playing around with idea of graphs

--- Project/temp.c
+++ Project/temp.c
@@ -1,138 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <graphics.h>
 #include <conio.h>
-#include <stdlib.h>
 
-//Graph Data Structure ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//predefined variables.
 static int radius = 5;
 static int startx = 20;
 static int starty = 20;
 static int endx = 420;
 static int endy = 420;
 static int unit_cell;
-static int count = -1;
+static int count = 0;
 
-int tos = -1;
-int front = 0;
-int rear = -1;
+char key_order[500];
 
-int queue[100];
-int stack[100];
-int traversal_order[100];
-
-int stackEmpty(){
-    int status;
-    if (tos == -1){
-        status = 1; //true
-    }
-    else{
-        status = 0; //false
-    }
-    return status;
-}
-
-int stackFull(){
-    int status;
-    if (tos == MAXSIZE - 1){
-        status = 1; //true
-    }
-    else{
-        status = 0; //false
-    }
-    return status;
-}
-
-void push(int n){
-    if (isFull()){
-        exit(1);
-    }
-    else{
-        tos ++;
-        stack[tos] = n;
-    }
-}
-
-int pop(){
-    int value = 0;
-    if (isEmpty()){
-        exit(1);
-    }
-    else{
-        value = stack[tos];
-        tos--;
-    }
-    return value; ////
-}
-
-void search_stack_for(int n){
-    int i;
-    for (i = 0; i <= tos; i++){
-        if(stack[i] == n){
-			return 1;
-		}
-    }
-	return 0;
-}
-
-int queueFull(){
-    return (rear == MAXSIZE - 1 ? 1 : 0);
-}
-
-int queueEmpty(){
-    return (rear < front ? 1 : 0);
-}
-
-void enqueue(int n){
-    if(isFull()){
-        exit(1);
-    }
-    rear++;
-    queue[rear] = n;
-    return;
-}
-
-int dequeue(){
-    if(isEmpty()){
-        exit(1);
-    }
-    int data = queue[front];
-    front++;
-    return data;
-}
-
-int search_queue_for(int n)
-{
-    int i;
-    if(isEmpty()){
-        return 0;
-    }
-
-    for(i = front; i <= rear; i++){
-        if(queue[i] == n){
-			return 1;
-		}
-    }
-
-    return 0;
-}
-
-typedef struct coordinate{
+typedef struct graph_vertex{
     int x, y; // coordinate values
 
-    int flag; /*
-    0 = Standard Path node
-    1 = Start node
-    2 = Finish node */
-
-    int status; // visited or unvisited, used during traversal.
-
-	int proxy; // we will rename each node during traversal.
-
     //pointers to adjacent nodes
-	struct coordinate *north;
-	struct coordinate *east;
-	struct coordinate *south;
-	struct coordinate *west;
+	struct graph_vertex *north;
+	struct graph_vertex *east;
+	struct graph_vertex *south;
+	struct graph_vertex *west;
 
 } node;
 
@@ -142,20 +31,13 @@ node *temp = NULL;
 node *new_node = NULL;
 node *start = NULL;
 
-//Graph Operations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-int insertNode(int x_coord, int y_coord, char direction, int identifier){
+int insertNode(int x_coord, int y_coord, char direction){
     int status;
     new_node = (node*) malloc(sizeof(node));
 
     //data
     new_node->x = x_coord;
     new_node->y = y_coord;
-    if(identifier == 1 || identifier == 2){
-        new_node->flag = identifier;
-    }else{
-        new_node->flag = 0; // defaults to standard path.
-    }
 
     if (head == NULL){
         head = new_node; // first node of graph.
@@ -251,9 +133,6 @@ int insertNode(int x_coord, int y_coord, char direction, int identifier){
 
 int deleteGraph(){
 
-
-
-
     return 0; //indicating function executed with no forseen issues.
 }
 
@@ -262,7 +141,12 @@ node* getNode(){
 	return head;
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+struct coordinate{
+	int x;
+	int y;
+};
+
+struct coordinate vertex[500];
 
 int determine_orientation(int old_x, int old_y, int x, int y){
 	int orientation;
@@ -285,273 +169,8 @@ int determine_orientation(int old_x, int old_y, int x, int y){
 	return orientation;
 }
 
-int check(int x, int y){
-	if(x < startx || y < starty || x > endx || y > endy)
-		return 0;
-	else
-		return 1;
-}
-
-int determine_id(int x, int y){
-	int status;
-	if(x == startx && y == starty){
-		status = 1;
-	}else if(x == endx && y == endy){
-		status = 2;
-	}else{
-		status = 0;
-	}
-	return status;
-}
-
-void draw_maze(){
-	char key;
-	int status, check_edge, id;
-	int n, u, loop = 1;
-	int x, y, i;
-
-	node* vertex;
-
-	cleardevice();
-	delay(10);
-	printf("\nMaze Size: <1> 2x2 <2> 4x4 <3> 5x5 <4> 8x8 <5> 10x10 ");
-	while(loop == 1){
-		key = getch();
-		switch(key){
-			case '1':
-				n = 2;
-				loop = 0;
-			break;
-
-			case '2':
-				n = 4;
-				loop = 0;
-			break;
-
-			case '3':
-				n = 5;
-				loop = 0;
-			break;
-
-			case '4':
-				n = 8;
-				loop = 0;
-			break;
-
-			case '5':
-				n = 10;
-				loop = 0;
-			break;
-
-			default:
-				loop =1;
-		}
-	}
-	cleardevice();
-	unit_cell = (endx - startx)/n;
-
-	loop = 1;
-
-	circle(startx, starty, radius);
-	circle(endx, endy, radius);
-
-	x = startx;
-	y = starty;
-	id = determine_id(x, y);
-	insertNode(x, y, 'x', id);
-
-	moveto(startx, starty);
-
-	while(loop == 1){
-		status = 0;
-		check_edge = 0;
-		id = 0;
-
-		key = getch();
-
-		switch(key){
-			case 'a':
-				if(check(x-unit_cell, y) == 1){
-					count++;
-					x = x-unit_cell;
-					y = y;
-					status = 1;
-				}
-			break;
-
-			case 'w':
-				if(check(x, y-unit_cell) == 1){
-					count++;
-					x = x;
-					y = y-unit_cell; 1;
-				}
-			break;
-
-			case 'd':
-				if(check(x+unit_cell, y) == 1){
-					count++;
-					x = x+unit_cell;
-					y = y;
-					status = 1;
-				}
-			break;
-
-			case 's':
-				if(check(x, y+unit_cell) == 1){
-					count++;
-					x = x;
-					y = y+unit_cell;
-					status = 1;
-				}
-			break;
-			case 27: // esc key resets the whole thing.
-				cleardevice();
-				circle(startx, starty, radius);
-				circle(endx, endy, radius);
-				count = 0;
-				deleteGraph();
-				moveto(startx, starty);
-				x = startx;
-				y = starty;
-				status = 1;
-			break;
-
-			default:;
-		}
-
-		if(status == 1){
-			id = determine_id(x, y);
-			check_edge = insertNode(x, y, key, id);
-			vertex = getNode();
-
-			if(check_edge == 1 || check_edge == 0){
-				lineto(vertex->x, vertex->y);
-			}else{
-				moveto(vertex->x, vertex->y);
-			}
-		}
-
-		if(x == endx && y == endy){
-			head->flag = 2;
-			head = start;
-			loop =0;
-		}
-	}
-}
-
-void bfs_span_tree(){
-	int node_number = 0;
-	int loop = 1;
-	int i = -1;
-	int j;
-	int next, hold;
-	node* vertex;
-
-	front = 0;
-	rear = -1;
-
-	while(loop == 1){
-		vertex = head;
-		j=0;
-
-		if(vertex->status!=2){
-			node_number++;
-			vertex->proxy = node_number;
-			vertex->status = 2;
-			i++
-			traversal_order[i] = vertex->proxy;
-
-			if(vertex->north != NULL){
-				ptr = vertex->north;
-				if(ptr->status!=1 && ptr->status!=2){
-					node_number++;
-					ptr->proxy = node_number;
-					ptr->status = 1; //visited
-					enqueue(node_number);
-				}
-			}
-			if(vertex->east != NULL){
-				ptr = vertex->east;
-				if(ptr->status!=1 && ptr->status!=2){
-					node_number++;
-					ptr->proxy = node_number;
-					ptr->status = 1; //visited
-					enqueue(node_number);
-				}
-			}
-			if(vertex->west != NULL){
-				ptr = vertex->west;
-				if(ptr->status!=1 && ptr->status!=2){
-					node_number++;
-					ptr->proxy = node_number;
-					ptr->status = 1; //visited
-					enqueue(node_number);
-				}
-			}
-			if(vertex->south != NULL){
-				ptr = vertex->south;
-				if(ptr->status!=1 && ptr->status!=2){
-					node_number++;
-					ptr->proxy = node_number;
-					ptr->status = 1; //visited
-					enqueue(node_number);
-				}
-			}
-
-			if(queueEmpty){
-				loop = 0;
-			}else{
-				next = dequeue();
-				hold = next;
-
-				while(1){
-					if(head->north->proxy == next){
-						head = head->north;
-						if (hold == next){
-							break;
-						}else{
-							next = hold;
-						}
-					}else if(head->east->proxy == next){
-						head = head->east;
-						if (hold == next){
-							break;
-						}else{
-							next = hold;
-						}
-					}else if(head->west->proxy == next){
-						head = head->west;
-						if (hold == next){
-							break;
-						}else{
-							next = hold;
-						}
-					}else if(head->south->proxy == next){
-						head = head->south;
-						if (hold == next){
-							break;
-						}else{
-							next = hold;
-						}
-					}else{
-						next = traversal_order[j];
-						j++;
-					}
-				}
-			}
-		}
-	}
-
-	head = start;
-}
-
-void maze(int m){
+void maze(){
 	int i = 0;
-	int n = 0;
-	int next ;
-	int visited[100];
-
-	tos = -1;
 
 	//making the maze border
 	setcolor(RED);
@@ -559,44 +178,149 @@ void maze(int m){
 	rectangle(startx - 10, starty - 10, endx + 10, endy + 10); // encloses a square of dimension 400 x 400
 
 	//making maze path
-	setcolor(LIGHTGRAY); // code : 7
-	moveto(head->x, head->y);
-	visited[n] = head->proxy;
-	i++; n++;
-	push(head->proxy);
-
-	while(1){
-		if(isVisited(head->north->proxy) == 1){
-
-		}else if
-	}
-
-
-
+	setcolor(LIGHTGRAY); // color code : 7
+	moveto(startx, starty);
 
 	for(i = 0; i < count+1 ; i ++){
 		setlinestyle(0, 0, 1);
-		key = key_order[count];
-		switch (key) {
-			case 'w':
+		lineto(vertex[i].x, vertex[i].y);
+	}
+}
+
+int check(int x, int y){
+	if(x < startx || y < starty || x > endx || y > endy)
+		return 0;
+	else
+		return 1;
+}
+
+void draw_maze(){
+	char key, status = 0; // to store user key input.
+	int n; // making an nxn maze
+	int loop = 1;
+	int x, y;
+
+	cleardevice();
+	delay(10);
+
+	//Asking the user to select the appropriate maze size.
+	printf("\nMaze Size: <1> 2x2 <2> 4x4 <3> 5x5 <4> 8x8 <5> 10x10 <6> 16x16 ");
+
+	while(loop == 1){
+		key = getch();
+		switch(key){
+			case '1': //2x2 maze
+				n = 2;
+				loop = 0;
 			break;
 
+			case '2': //4x4 maze
+				n = 4;
+				loop = 0;
+			break;
+
+			case '3': //5x5 maze
+				n = 5;
+				loop = 0;
+			break;
+
+			case '4': //8x8 maze
+				n = 8;
+				loop = 0;
+			break;
+
+			case '5': //10x10 maze
+				n = 10;
+				loop = 0;
+			break;
+
+			case '6': //16x16 maze
+				n = 16;
+				loop = 0;
+			break;
+
+			default: // continue asking till the user gives a valid imput
+				loop =1;
+		}
+	}
+	unit_cell = (endx - startx)/n;
+
+	//Asking user to use the wasd keys as arrow keys to draw the maze.
+	cleardevice();
+	loop = 1;
+	circle(startx, starty, radius);
+	circle(endx, endy, radius);
+
+	moveto(startx, starty);
+	x = startx;
+	y = starty;
+	vertex[count].x = x;
+	vertex[count].y = y;
+
+	while(loop == 1){
+		key = getch();
+		switch(key){
 			case 'a':
+				if(check(x-unit_cell, y) == 1){
+					//the check function checks if the next coordinate is withing maze bounds.
+					x = x-unit_cell;
+					y = y;
+                    status = 1;
+				}
 			break;
 
-			case 's':
+			case 'w':
+				if(check(x, y-unit_cell) == 1){
+					x = x;
+					y = y-unit_cell;
+                    status = 1;
+				}
 			break;
 
 			case 'd':
+				if(check(x+unit_cell, y) == 1){
+					x = x+unit_cell;
+					y = y;
+                    status = 1;
+				}
 			break;
+
+			case 's':
+				if(check(x, y+unit_cell) == 1){
+					x = x;
+					y = y+unit_cell;
+                    status = 1;
+				}
+			break;
+			case 27: // ASCII for esc key, basically enables re-draw
+				cleardevice();
+				circle(startx, starty, radius);
+				circle(endx, endy, radius);
+				x = startx;
+				y = starty;
+				moveto(x, y);
+			break;
+
+			default:;
+		}
+
+		if(status == 1){
+            count++;
+            key_order[count] = key;
+            vertex[count].x = x;
+            vertex[count].y = y;
+            lineto(x, y);
+        }
+		if(x == endx && y == endy){
+			loop =0;
 		}
 	}
 }
 
-int bot_color(int x, int y){
+int bot(int x, int y){
 
 	int status;
-	circle(x, y, radius);
+	circle(x, y, radius); // a circular bot
 
 	if(x == startx && y == starty){
 		setfillstyle(SOLID_FILL, YELLOW);
@@ -618,12 +342,12 @@ int bot_color(int x, int y){
 	return status;
 }
 
-int refresh(int x, int y){
+int refresh(int x, int y){ //redraws all graphic elements in each frame
 	int status;
 
 	cleardevice();
 
-	status = bot_color(x, y);
+	status = bot(x, y);
 
 	maze();
 
@@ -633,6 +357,13 @@ int refresh(int x, int y){
 }
 
 void left_wall_algorithm(){
+	/*
+	direction priority:
+	1. left
+	2. forward
+	3. right
+	4. turn around
+	*/
 	int find = 1; //controlls the loop below
 	int status; //holds the bot status
 	int orientation;
@@ -713,27 +444,64 @@ void left_wall_algorithm(){
 		status = refresh(x, y);
 		if(status == 2 || status == 13)
 		{
-			break;
+			find = 0;
 		}
 	}
 }
 
-int main(){
+void make_graph(){
+    char key;
+	int check_edge;
+	int n, u;
+	int x, y, i;
+
+	x = startx;
+	y = starty;
+	insertNode(x, y, 'x');
+
+	for(i = 0; i <=count; i++){
+		check_edge = 0;
+
+		key = key_order[i];
+
+		x = vertex[count].x;
+        y = vertex[count].y;
+
+		check_edge = insertNode(x, y, key);
+		ptr = getNode();
+
+		if(check_edge == 1 || check_edge == 0){
+			lineto(ptr->x, ptr->y);
+		}else{
+			moveto(ptr->x, ptr->y);
+		}
+	}
+    head = start;
+}
+
+void bfs(){
+
+}
+
+int main()
+{
 	int gd = DETECT, gm, wait_for_key = 1, n;
 	char key;
 
 	initgraph(&gd, &gm, "c:\\turboc3\\bgi");
 
-	// Darwing a maze.
+	//ask user to draw maze
 	draw_maze();
 
-	// determining traversal routes, i.e,
-	// spanning tree using BFS.
-	bfs_span_tree();
+    make_graph();
 
+	//use the user-drawn maze
 	maze();
+
+	//load starting frame.
 	refresh(startx, starty);
 
+	//wait for user input to start bot.
 	while (wait_for_key == 1) {
 		key = getch();
 		switch(key)
--- Test.c
+++ Test.c
@@ -1,74 +1,112 @@
-#include<string.h>
-    #include<stdlib.h>
-    #include<stdio.h>
-    #define max 10
-    char stackQ[max][256];
-    int stackAns[max];
-    int top=-1;
-    void pushAns(int a)
+// using linked list as a replacement for arrays
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void insert(int, int);
+void delete(int);
+void display();
+
+typedef struct list
+{
+    char data;
+    struct list *next_data;
+    struct list *prev_data;
+} d_list;
+
+d_list *list_head = NULL;
+d_list *new_list_element;
+d_list *ptr;
+d_list *temp;
+
+char get 
+
+void insert(int n) // insertion at end
+{
+	newNode = (struct node*) malloc(sizeof(struct node));
+    newNode->data = n;
+    int i = 1, position;
+
+    if (list_head == NULL){
+		head = new_list_element;
+		new_list_element->prev_data = NULL;
+		new_list_element->next_data = NULL;
+	}else{
+		ptr = head;
+		while(ptr->next != NULL){
+			ptr = ptr-next_data;
+		}
+		//at this point the ptr points to last data of list.
+		ptr->next = new_list_element;
+		new_list_element->prev_data = ptr;
+		new_list_element->next_data = NULL;
+	}
+}
+
+void delete(int pos)
+{
+    int position, i = 1;
+    switch (pos)
     {
-    top=top+1;
-    stackAns[top]=a;
+        case 1:
+            temp = head;
+            head = head->next_data;
+            if(head != NULL)
+            {
+                head->prev_data = NULL; //making the new first element point to null.
+            }
+            free(temp);
+        break;
+
+        case 2:
+            ptr = head;
+            if(head->next_data == NULL){
+                free(ptr);
+                head = NULL;
+            }
+            else{
+                while(ptr->next_data != NULL)
+                {
+                    temp = ptr;
+                    ptr = ptr->next_data;
+                }
+                temp->next_data = NULL;
+                free(ptr);
+            }
+        break;
+
+        default:
+            system("cls");
+            printf("\nDelete position : ");
+            scanf("%d", &position);
+            if(position < 1){
+                printf("\nPosition begins at 1.");
+                break;
+            }
+            if(position == 1){
+                delete(1);
+                break;
+            }
+            ptr = head;
+            while(i < position)
+            {
+                temp = ptr;
+                ptr = ptr->next_data;
+                i++;
+            }
+            if(ptr == NULL){
+                printf("\nNOT FOUND\n");
+                getch();
+                break;
+            }
+            temp->next_data = ptr->next_data;
+            temp->next_data->prev_data = temp;
+            free(ptr);
     }
-    int main()
-    {
-    printf("A miniproject brought to you by\n Bhawana adhikari and Rensa neupane\n");
-    printf("A quiz using STACK as the data structure to pop the first and most valued question at last.\n");
-    int points=0,i,ans;
-    char name[100];
-    char stackQ[max][256] = {
-        "Final Question!!\nWhich of the following is linear data structure?\n1)tree\t2)graph\t3)none\t4)array",
-        "9th Question!!\nWhich of the following is non-linear data structure?\n1)none\t2)array\t3)list\t4)Stack",
-        "8th Question!!\nStack is based on which principle?\n1)LIFO\t2)FIFO\t3)PUSH\t4)none",
-        "7th Question!!\nWe use theta notation for \n1)worst case\t2)Best case\t3)average case\t4)none",
-        "6th Question!!\nMinimum how many stacks are needed to implement a queue\n1)2\t2)12\t3)0\t4)10",
-        "5th Question!!\n Queue is based on which principle? \n1)FIFO\t2)LIFO\t3)PULL\t4)none",
-        "4th Question!!\nFunction is to specify the data type\n1)typedef\t2)arr\t3)none\t4)id",
-        "3rd Question!!\nA function which calls itself is called \n1)iteration\t2)algorithm\t3)recursion\t4)none",
-        "2nd Question!!\nrecursion is based on \n1)stack\t2)queue\t3)none\t4)tree",
-        "1st Question!!\nLinear Search is best for \n1)large arrays\t2)small arrays\t3)none\t4)sorted arrays"};
-    pushAns(4);                //push 10 ans.
-    pushAns(1);
-    pushAns(1);
-    pushAns(3);
-    pushAns(1);
-    pushAns(1);
-    pushAns(1);
-    pushAns(3);
-    pushAns(1);
-    pushAns(1);
-    printf("\nWelcome to Quiz.\n");
-    printf("Enter your name.  \n");
-    scanf("%s",name);
-    printf("Welcome %s\n",name);
-    for(i=9;i>=0;i--)
-    {
-        printf("\n\nThis question is for %d points\n",(10-i)*10);
-        printf("%s\n",stackQ[i]);
-        printf("Enter your ans :\n");
-        scanf("%d",&ans);
-        if(ans==stackAns[i])
-        {
-            points=points+(10-i)*10;
-            printf("Congrats!!!\t You earn %d points\n",(10-i)*10);
-        }
-        else if(ans==0)
-        {
-            printf("You chose to quit\n");
-            break;
-        }
-        else
-        {
-            printf("Sorry, the correct answer is %d. \n",stackAns[i]);
-            break;
-        }
-        }
-        if(points==550)
-        {
-            printf("You are the winner of the quizeria with %d points!!!\n",points);
-        }
-        else {
-            printf("Sorry, the correct answer is %d. \n",stackAns[i]);
-        }
-         printf("You scored %d points!!!\n",points);
-    }
+}
+
+
+int main(){
+
+	return 0;
+}
